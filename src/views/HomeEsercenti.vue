5fe7bc87b4b1ec723d3185aad4e84add75006496
PiervitoCreanza <p.creanza@protonmail.com>
2020-12-28 00:01:35 +0100

    Add HomeEsercenti page

---
+++ src/views/HomeEsercenti.vue
@@ -0,0 +1,27 @@
+<template>
+  <qr-stream v-if="!isQrLoaded" @decode="onDecode"></qr-stream>
+</template>
+<script>
+import { QrStream } from "vue3-qr-reader";
+import router from "@/router";
+console.log("data", QrStream);
+export default {
+  data() {
+    return {
+      isQrLoaded: false,
+    };
+  },
+  methods: {
+    onDecode(decodedString) {
+      if (decodedString) {
+        router.push("/viewcard/" + decodedString);
+        this.isQrLoaded = false;
+      }
+      console.log(decodedString);
+    },
+  },
+  components: {
+    QrStream,
+  },
+};
+</script>
