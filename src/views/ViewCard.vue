4b892d0e58a348f45493f18f2bb7a8d454c4390e
PiervitoCreanza <p.creanza@protonmail.com>
2020-12-28 00:03:27 +0100

    Implement redirects, new error handling and polish

--- src/views/ViewCard.vue
+++ src/views/ViewCard.vue
@@ -3,47 +3,35 @@
     <div class="container my-auto">
       <div class="row">
         <div class="col-12 col-md-6 my-auto">
-          <div v-if="this.permissionError">
+          <div v-if="error.isError">
             <div class="error-message message py-4 px-5 mx-auto">
-              <h1>Permessi non sufficienti</h1>
+              <h1>{{ error.message.title }}</h1>
+              <button
+                v-if="error.code === 'permission-denied'"
+                @click="signIn"
+                class="btn btn-large btn-light shadow text-center"
+              >
+                Accedi come esercente
+              </button>
             </div>
           </div>
-          <div v-else-if="this.card.exists && !this.card.suspended">
+
+          <div v-else-if="card.isLoaded && !card.suspended">
             <div class="success-message message py-4 px-5 mx-auto">
               <h1>Carta autorizzata</h1>
             </div>
-            <h5 class="mt-3">Proprietario: {{ this.card.displayName }}</h5>
+            <h5 class="mt-3">Proprietario: {{ card.displayName }}</h5>
           </div>
-          <div v-else-if="!this.card.exists">
-            <div class="error-message message py-4 px-5 mx-auto">
-              <h1>Card non valida</h1>
-            </div>
-          </div>
-          <div v-else-if="this.card.exists && this.card.suspended">
+
+          <div v-else-if="card.isLoaded && card.suspended">
             <div class="error-message message py-4 px-5 mx-auto">
               <h1>Card sospesa o scaduta</h1>
             </div>
           </div>
         </div>
+
         <div class="col-12 col-md-6 my-auto mt-4 mt-md-0">
-          <div v-if="this.permissionError" class="text-md-right">
-            <h1 class="display-4">Cosa significa?</h1>
-            <p>
-              Non hai i permessi per visualizzare queste informazioni. Se sei un
-              esercente esegui l'accesso. Nel caso dovessi continuare a
-              riscontrare problemi contatta l'assistenza.
-            </p>
-            <button
-              @click="signIn"
-              class="btn btn-large btn-light shadow text-center"
-            >
-              Accedi come esercente
-            </button>
-          </div>
-          <div
-            v-else-if="this.card.exists && this.card.suspended"
-            class="text-md-right"
-          >
+          <div v-if="card.isLoaded && card.suspended" class="text-md-right">
             <h1 class="display-4">Cosa significa?</h1>
             <p>
               La card scansionata è scaduta oppure è stata sospesa. L'utente che
@@ -51,24 +39,30 @@
               contatta l'assistenza.
             </p>
           </div>
-          <div v-else-if="!this.card.exists" class="text-md-right">
+
+          <div v-else-if="error.isError" class="text-md-right">
             <h1 class="display-4">Cosa significa?</h1>
-            <p>
-              La card scansionata non esiste nei nostri sistemi, potrebbe
-              trattarsi di una carta falsa o duplicata. L'utente che la possiede
-              non ha diritto a eventuali sconti. Per approfondimenti contatta
-              l'assistenza.
-            </p>
+            <p>{{ error.message.description }}</p>
           </div>
-          <div v-else-if="this.card.exists && !this.card.suspended">
-            <h1 class="display-4">Ultima scansione</h1>
-            <p v-if="this.card.lastScan" class="mb-0">
-              L'ultima volta che questa card è stata utilizzata presso il tuo
-              negozio era il:
-            </p>
-            <div v-if="this.card.lastScan">
-              <h1>{{ this.card.lastScan }}</h1>
-            </div>
+
+          <div v-else-if="card.isLoaded && !card.suspended">
+            <template v-if="card.lastScan">
+              <h1 class="display-4">
+                Ultima scansione
+              </h1>
+              <p class="mb-0">
+                L'ultima volta che questa card è stata utilizzata presso il tuo
+                negozio era il:
+              </p>
+              <h1>{{ card.lastScan }}</h1>
+            </template>
+
+            <button
+              @click="redirectToScan"
+              class="btn btn-large btn-light shadow text-center mt-4"
+            >
+              Scansiona un'altra carta
+            </button>
           </div>
         </div>
       </div>
@@ -78,59 +72,74 @@
 
 <script>
 import { db, auth } from "../assets/js/firebase";
+import router from "@/router";
+import { formatError } from "@/assets/js/errorHandler";
 export default {
   created() {
     auth.onAuthStateChanged((user) => {
-      console.log(user.uid);
+      if (!user) {
+        return (this.error = formatError("permission-denied"));
+      }
+
       db.collection("users")
         .doc(this.$route.params.id)
         .get()
         .then((res) => {
-          if (res.exists) {
-            let data = res.data();
-            console.log(data);
-            this.card = {
-              exists: true,
-              suspended: data.suspended,
-              displayName: data.displayName,
-              lastScan:
-                data.lastScan[user.uid] &&
-                data.lastScan[user.uid]
-                  .toDate()
-                  .toISOString()
-                  .split("T")[0],
-            };
-            db.collection("users")
-              .doc(data.uid)
-              .update({
-                lastScan: {
-                  [user.uid]: new Date(),
-                },
-              });
-          } else {
-            this.card.exists = false;
+          if (!res.exists) {
+            return (this.error = formatError("cardDoesNotExist"));
           }
+
+          let data = res.data();
+          console.log(data);
+          this.card = {
+            isLoaded: true,
+            suspended: data.suspended,
+            displayName: data.displayName,
+            lastScan:
+              data.lastScan &&
+              data.lastScan[user.uid]
+                .toDate()
+                .toISOString()
+                .split("T")[0],
+          };
+          db.collection("users")
+            .doc(data.uid)
+            .update({
+              lastScan: {
+                [user.uid]: new Date(),
+              },
+            });
         })
         .catch((err) => {
-          this.permissionError = true;
-          console.log(err);
-          console.log("permessi non sufficienti");
+          this.error = formatError(err.code);
         });
     });
   },
   data() {
     return {
       card: {
-        exists: false,
+        isLoaded: false,
         suspended: false,
         displayName: null,
         lastScan: null,
+      },
+      error: {
+        isError: false,
+        message: {
+          title: null,
+          description: null,
+        },
       },
       permissionError: false,
     };
   },
   methods: {
-    signIn() {},
+    signIn() {
+      router.push("/esercenti");
+    },
+    redirectToScan() {
+      router.push("/home-esercenti");
+    },
   },
 };
 </script>
