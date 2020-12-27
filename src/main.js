fb08fa20e1d9ffd7347c17f8657e3c52644fe7b6
PiervitoCreanza <p.creanza@protonmail.com>
2020-12-27 23:56:20 +0100

    Add bootstrap via npm

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "./assets/css/bootstrap.min.css";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/global.scss";
 import { auth } from "./assets/js/firebase";
 
