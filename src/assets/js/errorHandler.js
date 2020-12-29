59deb219d523be195311179d5e8b5327824af779
PiervitoCreanza <p.creanza@protonmail.com>
2020-12-29 09:28:01 +0100

    Add new errors

--- src/assets/js/errorHandler.js
+++ src/assets/js/errorHandler.js
@@ -14,6 +14,26 @@ const errorMessages = {
     description:
       "La card scansionata non esiste nei nostri sistemi, potrebbe trattarsi di una carta falsa o duplicata. L'utente che la possiede non ha diritto a eventuali sconti. Per approfondimenti contatta l'assistenza.",
   },
+  NotAllowedError: {
+    title: "Accesso alla fotocamera negato",
+    description:
+      "Non hai concesso al sito di utilizzare la fotocamera. Se sei su un dispositivo mobile seleziona: 'Impostazioni --> Impostazioni sito --> Videocamera' e consenti l'accesso a card.lecosimo.it. Se sei su un computer premi sul simbolo della videocamera nella barra di ricerca e concedi l'accesso.",
+  },
+  InsecureContextError: {
+    title: "Connessione non sicura",
+    description:
+      "Non sei sulla versione https del sito. Cerca https://card.lecosimo.it",
+  },
+  NotFoundError: {
+    title: "Nessuna fotocamera rilevata",
+    description:
+      "A quanto pare il tuo dispositivo non è provvisto di una fotocamera. Verifica che sia collegata e riconosciuta dal tuo dispositivo",
+  },
+  StreamApiNotSupportedError: {
+    title: "Browser non supportato",
+    description:
+      "A quanto pare il tuo browser non è supportato da De Giorgi's Card. Prova a cambiare browser. Se sei su ios utilizza Safari.",
+  },
 };
 
 export const formatError = (errorCode) => {
