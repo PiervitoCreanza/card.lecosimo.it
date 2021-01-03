bd99e7714d953b988b0fe706e5eee8a2e4d8321e
PiervitoCreanza <p.creanza@protonmail.com>
2021-01-03 23:17:07 +0100

    Add bundle analyzer and implement code split

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new BundleAnalyzerPlugin(),
+    ] /*
+    optimization: {
+      runtimeChunk: "single",
+      splitChunks: {
+        chunks: "all",
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1];
+              return `npm.${packageName.replace("@", "")}`;
+            },
+          },
+        },
+      },
+    },*/,
+  },
+};
