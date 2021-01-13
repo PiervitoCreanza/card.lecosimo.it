8b533c89cff8d7806fc0a1cd5077290738ad534d
PiervitoCreanza <p.creanza@protonmail.com>
2021-01-13 08:44:33 +0100

    Fix debug

--- src/store/index.js
+++ src/store/index.js
@@ -7,10 +7,12 @@ const initialState = () => {
   return { user: null, error: null, isLoading: false };
 };
 
+const debug = process.env.NODE_ENV !== "production";
+const plugins = debug ? [createLogger()] : [];
 export const store = createStore({
   state: initialState(),
   mutations: mutations,
   actions: actions,
   getters: getters,
-  plugins: [createLogger()],
+  plugins,
 });
