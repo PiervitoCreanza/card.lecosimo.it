7158dca63feb4b64c1fb58f6fb59473d63a93dde
PiervitoCreanza <p.creanza@protonmail.com>
2021-01-14 22:29:54 +0100

    Add loading screen

--- src/views/HomeEsercenti.vue
+++ src/views/HomeEsercenti.vue
@@ -1,14 +1,17 @@
 <template>
   <section class="scene mt-3 mt-md-0">
     <div class="container my-auto">
+      <LoadingScreen
+        class="mt-5 mt-md-0"
+        v-if="!error.isError && !isScannerLoaded"
+        loadingText="Caricamento fotocamera"
+      />
       <div class="row">
         <div class="col-12 col-md-6">
-          <template v-if="!isQrLoaded && !error.isError">
+          <div v-show="isScannerLoaded && !error.isError">
             <h1 class="mb-4">Inquadra il QR Code</h1>
             <qr-stream @decode="onDecode" @init="onInit"></qr-stream>
-          </template>
-
-          <h1 v-if="isScannerLoaded">Caricamento in corso..</h1>
+          </div>
           <div
             v-if="error.isError"
             class="error-message message py-4 px-5 mx-auto"
@@ -30,6 +33,7 @@
 import { QrStream } from "vue3-qr-reader";
 import router from "@/router";
 import { formatError } from "@/assets/js/errorHandler";
+import LoadingScreen from "@/components/LoadingScreen.vue";
 export default {
   data() {
     return {
@@ -53,24 +57,24 @@ export default {
       }
       console.log(decodedString);
     },
-    async onInit(promise) {
-      this.isScannerLoaded = false;
-      console.log("hello");
-      try {
-        const { capabilities } = await promise;
-        console.log(capabilities);
-        // successfully initialized
-        this.isScannerLoaded = false;
-      } catch (error) {
-        this.error = formatError(error.name);
-      } finally {
-        console.log("working");
-        //
-      }
+    async onInit(waitForLoading) {
+      console.log("LOADING_QRSCANNER");
+
+      waitForLoading
+        .then((capabilities) => {
+          console.log("SUCCESS_QRSCANNER");
+          console.log(capabilities);
+          this.isScannerLoaded = true;
+        })
+        .catch((err) => {
+          console.log("ERROR_QRSCANNER");
+          this.error = formatError(err.name);
+        });
     },
   },
   components: {
     QrStream,
+    LoadingScreen,
   },
 };
 </script>
