81f5c27174413c61b54a4a6ee7d67a3ec779450e
PiervitoCreanza <p.creanza@protonmail.com>
2021-01-28 18:37:31 +0100

    Import bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/global.scss";
 import { store } from "./store";
